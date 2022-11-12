7cf3b44239c0dd4a9cbde18e9e573835a615fc69
bogdanovTeam7 <bogdanov@t-email.hu>
2022-11-12 18:37:01 +0100

    init

---
+++ Tom-SB-Rest-AutorentCars/src/main/java/pti/sb_rest_autorentcars/TomSbRestAutorentCarsApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_rest_autorentcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomSbRestAutorentCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomSbRestAutorentCarsApplication.class, args);
+	}
+
+}
